32ce0e8cc94adedf05f56e49aa8d674dc90d016e
MatosLarissa <larissamatos88@outlook.com>
2022-11-15 18:02:50 -0300

    feat: model type card

---
+++ src/model/card/typeCard.ts
@@ -0,0 +1,10 @@
+type TypeCard = {
+    id: string,
+    cardNumber: string,
+    cardCustomerName: string,
+    cardExpiration: Date,
+    cardPassword: string,
+    cvvCode: Date,
+}
+
+export default TypeCard;
