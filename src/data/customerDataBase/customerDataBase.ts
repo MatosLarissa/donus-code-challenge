6834ef99dba4e49164fe2930b1c7f08a1d264178
MatosLarissa <larissamatos88@outlook.com>
2022-11-15 17:21:47 -0300

    feat: create customer dataBase

---
+++ src/data/customerDataBase/customerDataBase.ts
@@ -0,0 +1,24 @@
+import CustomerRepositoryInterface from "../../business/repositoryInterface/customer/customerRepositoryInterface"
+import Customer from "../../model/customer/customer"
+import BaseDataBase from "../baseDataBase/baseDataBase"
+
+export default class CustomerDataBase extends BaseDataBase implements CustomerRepositoryInterface {
+    private TABLE_NAME = "customer"
+
+    createCustomer = async (customer: Customer) => {
+        try {
+            await CustomerDataBase
+                .connection(this.TABLE_NAME)
+                .insert({
+                    "id": customer.getId(),
+                    "fullName": customer.getFullName(),
+                    "cpf": customer.getCpf(),
+                    "email": customer.getEmail(),
+                    "password": customer.getPassword(),
+                    "created": customer.getCreated()
+                })
+        } catch (error: any) {
+            throw new Error(error.message || error.sqlMessage)
+        }
+    }
+}
