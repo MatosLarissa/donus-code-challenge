c9b296289c96bc50f050f5b25d3028a9369f21ac
MatosLarissa <larissamatos88@outlook.com>
2022-11-15 19:29:47 -0300

    feat: create payable controller

---
+++ src/controller/payableController/payableController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import PayableBusiness from "../../business/payable/payableBusiness";
+import PayableDataBase from "../../data/payableDataBase/payableDataBase";
+import PayableInputDTO from "../../model/playable/playableDto/payableInputDto";
+
+export default class PayableController {
+
+    private payableBusiness: PayableBusiness
+
+    constructor() {
+        this.payableBusiness = new PayableBusiness(new PayableDataBase)
+    }
+
+    createPayable = async (req: Request, res: Response) => {
+        const input: PayableInputDTO = {
+            token: req.headers.authorization,
+            paymentMethod: req.body.paymentMethod,
+            value: req.body.value,
+            description: req.body.description,
+            cardNumber: req.body.cardNumber,
+            cvv: req.body.cvv,
+        }
+
+        try {
+            const token = await this.payableBusiness.createPayable(input)
+
+            res.status(200).send({
+                message: "Compra realizada com sucesso!",
+                token
+            })
+
+        } catch (error: any) {
+            if (error.message) return res.status(400).send(error.message)
+            res.status(400).send("Erro no signup")
+        }
+    }
+}
