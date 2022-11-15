5428563b32e0db4d0d222963cd6c786c76605529
MatosLarissa <larissamatos88@outlook.com>
2022-11-15 17:45:46 -0300

    feat: model payable

---
+++ src/model/playable/payable.ts
@@ -0,0 +1,62 @@
+import paymentStatus from "../enums/paymentStatus"
+
+export default class Payable {
+    constructor(
+        private id: string,
+        private status: paymentStatus,
+        private paymentDate: string,
+        private createdDate: string,
+        private value: number,
+        private description: string,
+        private idCustomer: string
+
+    ) { }
+    static toPayableModel(customer: any): Payable {
+        return new Payable(customer.id, customer.status, customer.paymentDate, customer.createdDate, customer.amount, customer.description, customer.idCustomer)
+    }
+
+    getId() {
+        return this.id
+    }
+    getStatus() {
+        return this.status
+    }
+    getPaymentDate() {
+        return this.paymentDate
+    }
+    getCreatedDate() {
+        return this.createdDate
+    }
+    getValue() {
+        return this.value
+    }
+    getDescription() {
+        return this.description
+    }
+    getIdCustomer() {
+        return this.idCustomer
+    }
+
+    setId(id: string) {
+        this.id = id
+    }
+    setStatus(status: paymentStatus) {
+        this.status = status
+    }
+    setPaymentDate(paymentDate: string) {
+        this.paymentDate = paymentDate
+    }
+    setCreatedDate(createdDate: string) {
+        this.createdDate = createdDate
+    }
+    setValue(value: number) {
+        this.value = value
+    }
+    setDescription(description: string) {
+        this.description = description
+    }
+    setIdCustomer(idCustomer: string) {
+        this.idCustomer = idCustomer
+    }
+
+}
