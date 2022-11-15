5906568f332ada6b58f014456691d5efcdca9035
MatosLarissa <larissamatos88@outlook.com>
2022-11-15 17:38:06 -0300

    feat: install and enable cors

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@
 import express from "express";
 import { AddressInfo } from "net";
 import customerRouter from "./routes/routes";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/user", customerRouter)
 
