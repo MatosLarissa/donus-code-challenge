910b1e9d7ce45fdfbce80ea4f8f064d077c6ad2d
MatosLarissa <larissamatos88@outlook.com>
2022-11-15 17:53:37 -0300

    feat: model transaction

---
+++ src/model/transaction/transaction.ts
@@ -0,0 +1,45 @@
+import paymentMethod from "../enums/paymentMethod";
+
+export default class Transaction {
+    constructor(
+        private id: string,
+        private cardNumber: string,
+        private cvv: string,
+        private idCustomer: string,
+        private paymentMethod: paymentMethod
+    ) { }
+
+    getId() {
+        return this.id
+    }
+    getCardNumber() {
+        return this.cardNumber
+    }
+    getCvv() {
+        return this.cvv
+    }
+    getIdCustomer() {
+        return this.idCustomer
+    }
+    getPaymentMethod() {
+        return this.paymentMethod
+    }
+
+    setId(id: string) {
+        this.id = id
+    }
+    setCardNumber(cardNumber: string) {
+        this.cardNumber = cardNumber
+    }
+    setCvv(cvv: string) {
+        this.cvv = cvv
+    }
+    setIdCustomer(idCustomer: string) {
+        this.idCustomer = idCustomer
+    }
+    setPaymentMethod(paymentMethod: paymentMethod) {
+        this.paymentMethod = paymentMethod
+    }
+}
+
+
