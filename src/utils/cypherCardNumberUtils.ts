d7ef28d21f6af7a2c29761980075ec074d34e2a2
MatosLarissa <larissamatos88@outlook.com>
2022-11-15 18:21:10 -0300

    feat: cypher card number utils

---
+++ src/utils/cypherCardNumberUtils.ts
@@ -0,0 +1,15 @@
+export class CypherCardNumberUtils {
+    cypherNumber(
+        card: string
+    ): string {
+        let coverNumber = [];
+        for (let i = 0; i < card.length; i++) {
+            if (i < card.length - 4) {
+                coverNumber.push("*");
+            } else {
+                coverNumber.push(card[i]);
+            }
+        }
+        return coverNumber.join("");
+    }
+}
