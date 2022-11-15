81d14e08f0b49eb1ed12730d0bdc31f7c62789cb
MatosLarissa <larissamatos88@outlook.com>
2022-11-15 17:24:42 -0300

    feat: get customer by email dataBase

--- src/data/customerDataBase/customerDataBase.ts
+++ src/data/customerDataBase/customerDataBase.ts
@@ -3,6 +3,7 @@ import Customer from "../../model/customer/customer"
 import BaseDataBase from "../baseDataBase/baseDataBase"
 
 export default class CustomerDataBase extends BaseDataBase implements CustomerRepositoryInterface {
+
     private TABLE_NAME = "customer"
 
     createCustomer = async (customer: Customer) => {
@@ -29,6 +30,30 @@ export default class CustomerDataBase extends BaseDataBase implements CustomerRe
                 .select()
                 .where({ cpf })
             return result.length ? result[0] : null
+        } catch (error: any) {
+            throw new Error(error.message || error.sqlMessage)
+        }
+    }
+
+    getCustomerByEmail = async (email: string) => {
+        try {
+            const queryResult: any = await CustomerDataBase
+                .connection(this.TABLE_NAME)
+                .select()
+                .where({ email })
+            if (queryResult[0]) {
+                const result = new Customer(
+                    queryResult[0].id,
+                    queryResult[0].fullName,
+                    queryResult[0].cpf,
+                    queryResult[0].email,
+                    queryResult[0].password,
+                    queryResult[0].created
+                )
+                return result
+            } else {
+                return null
+            }
         } catch (error: any) {
             throw new Error(error.message || error.sqlMessage)
         }
