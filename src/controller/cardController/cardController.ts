64fb5cdcd08545672fba4e0fa347b773c9b211cd
MatosLarissa <larissamatos88@outlook.com>
2022-11-15 18:39:43 -0300

    feat: create card controller

---
+++ src/controller/cardController/cardController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import { CardBusiness } from "../../business/card/cardBusiness";
+import CardDataBase from "../../data/cardDataBase/cardDataBase";
+import CardInputDTO from "../../model/card/cardDto/cardInputDto";
+
+export default class CardController {
+
+    private cardBusiness: CardBusiness
+    
+    constructor() {
+        this.cardBusiness = new CardBusiness(new CardDataBase)
+    }
+
+    createCard = async (req: Request, res: Response) => {
+
+        const input: CardInputDTO = {
+            token: req.headers.authorization,
+            cardCustomerName: req.body.cardCustomerName,
+            cardNumber: req.body.cardNumber,
+            cvv: req.body.cvv,
+        }
+
+        try {
+            const token = await this.cardBusiness.createCard(input)
+
+            res.status(200).send({
+                message: "Cartão criado com sucesso!",
+                token
+            })
+
+        } catch (error: any) {
+            if (error.message) return res.status(400).send(error.message)
+            res.status(400).send("Erro no signup")
+        }
+    }
+}
