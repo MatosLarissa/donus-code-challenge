48885f6f9dc7d03044ecda7bae9eb8e91b8bde4e
MatosLarissa <larissamatos88@outlook.com>
2022-11-15 18:11:39 -0300

    feat: rename card repository interface

--- src/business/repositoryInterface/card/cardRepositoryInterface.ts
+++ src/business/repositoryInterface/card/cardRepositoryInterface.ts
@@ -1,6 +1,6 @@
 import Card from "../../../model/card/card"
 
-export default interface CardRepository {
+export default interface CardRepositoryInterface {
     createCard(card: Card): Promise<void>
     getAllTransactionByCustomerId(idCustomer: string): Promise<Card | null>
 }
